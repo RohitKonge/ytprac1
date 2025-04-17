976045329ee89d544ecda4ff5244a1028bf257de
RohitKonge <https://github.com/RohitKonge>
2025-04-17 22:30:11 +0530

    2nd Commit

--- app.py
+++ app.py
@@ -65,4 +65,5 @@ def download():
         return f"Error: {str(e)}"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
