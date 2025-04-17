8fc5adc237c429e87ef55a430af1a2f5d5ce534a
RohitKonge <https://github.com/RohitKonge>
2025-04-17 22:48:29 +0530

    5th Commit

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import yt_dlp
 import os
 import uuid
 import time
+import tempfile
+import json
 
 app = Flask(__name__)
 
@@ -26,6 +28,21 @@ HTML = """
 </html>
 """
 
+# Create a function to generate cookie file
+def create_cookie_file():
+    temp_cookie_file = os.path.join(tempfile.gettempdir(), 'youtube_cookies.txt')
+    with open(temp_cookie_file, 'w') as f:
+        # Write header required for Netscape cookie file format
+        f.write("# Netscape HTTP Cookie File\n")
+        f.write("# https://curl.haxx.se/rfc/cookie_spec.html\n")
+        f.write("# This is a generated file!  Do not edit.\n\n")
+        
+        # Write the cookie in proper Netscape format:
+        # domain	domain_initial_dot	path	secure	expiry	name	value
+        f.write(".youtube.com\tTRUE\t/\tFALSE\t{}\tCONSENT\tYES+cb\n".format(int(time.time()) + 31536000))
+        f.write(".youtube.com\tTRUE\t/\tFALSE\t{}\tPREF\tf6=8\n".format(int(time.time()) + 31536000))
+    return temp_cookie_file
+
 @app.route("/")
 def index():
     return render_template_string(HTML)
@@ -37,12 +54,15 @@ def download():
         return "No URL provided", 400
 
     filename = f"{uuid.uuid4()}.mp4"
+    cookie_file = create_cookie_file()
+
     ydl_opts = {
         'outtmpl': filename,
         'format': 'best',
         'quiet': True,
         'noplaylist': True,
         'nocheckcertificate': True,
+        'cookiefile': cookie_file,
         'extractor_args': {
             'youtube': {
                 'player_client': ['android', 'web'],
@@ -65,16 +85,24 @@ def download():
         time.sleep(1)
 
         @after_this_request
-        def remove_file(response):
+        def cleanup(response):
             try:
                 os.remove(filename)
+                os.remove(cookie_file)  # Clean up the cookie file
             except Exception as e:
-                print(f"Error deleting file: {e}")
+                print(f"Error cleaning up files: {e}")
             return response
 
         return send_file(filename, as_attachment=True)
 
     except Exception as e:
+        # Clean up files in case of error
+        try:
+            os.remove(cookie_file)
+            if os.path.exists(filename):
+                os.remove(filename)
+        except:
+            pass
         return f"Error: {str(e)}"
 
 if __name__ == "__main__":
